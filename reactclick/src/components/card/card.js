c6bf435c70b1a13f912873855add58e5c99eb950
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-16 15:46:29 -0500

    adding description to image card

--- reactclick/src/components/card/card.js
+++ reactclick/src/components/card/card.js
@@ -7,13 +7,20 @@ function addTotal() {
     console.log(amountClicked);
 };
 
+const cardStyle = {
+    width: '200px',
+    height: '200px'
+  };
+
 function Card(props) {
     return (
         props.Characters.map(char => (
+            
             <button key={char.id} clicked="no" className="button" onClick={addTotal}>
-                <div className="Nope">
-                    <div className="img-container">
+                <div className="Nope" >
+                    <div className="img-container"style={(cardStyle)}>
                         <img alt={char.name} src={char.image} />
+                        <p>{char.about}</p>
                     </div>
                 </div>
             </button>
