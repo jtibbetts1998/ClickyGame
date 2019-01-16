23e610d3e689cb163853d8fba2efd4fdacb65c4e
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-16 08:54:47 -0500

    updates to styling and logic

--- reactclick/src/App.js
+++ reactclick/src/App.js
@@ -7,9 +7,45 @@ import characters from '.characters.json';
 import logo from './logo.svg';
 import './App.css';
 
+var aCStyle = {
+  fontSize: "12px",
+  fontStyle: "italic",
+  margin: "10px 10px 10px 10px",
+  bottom: "4"
+}
+
+var sectionStyle = {
+  width: "100%",
+  height: "100%",
+  backgroundImage: "url(" + background + ")",
+  color: "white",
+  textShadow: "2px 2px #ff0000"
+};
+
+var fontStyle = {
+  fontSize: "12px",
+  fontStyle: "italic"
+}
+
+const ColoredLine = ({ color }) => (
+  <hr
+  style={{
+    color: color,
+    backgroundColor: color,
+    height: 0.2
+  }}
+ />
+);
+
+
 class App extends Component {
   render() {
+  console.log("rendering");
+
     return (
+    <React.Fragment>
+
+    
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -26,6 +62,9 @@ class App extends Component {
           </a>
         </header>
       </div>
+
+
+      </React.Fragment>
     );
   }
 }
