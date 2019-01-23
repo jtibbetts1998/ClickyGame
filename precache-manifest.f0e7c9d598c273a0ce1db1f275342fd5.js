b99a3da481a988a40c9ae33a026228d7a4f1529b
jtibbetts1998 <justin.tibbetts98@gmail.com>
2019-01-23 12:54:13 -0500

    Updates

---
+++ precache-manifest.f0e7c9d598c273a0ce1db1f275342fd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b82d8fae66228bff721",
+    "url": "/CLickyGame/static/css/main.70c466e1.chunk.css"
+  },
+  {
+    "revision": "6b82d8fae66228bff721",
+    "url": "/CLickyGame/static/js/main.6b82d8fa.chunk.js"
+  },
+  {
+    "revision": "0de229b10717e96b1884",
+    "url": "/CLickyGame/static/js/1.0de229b1.chunk.js"
+  },
+  {
+    "revision": "a1c1c6e2bf9949668f58",
+    "url": "/CLickyGame/static/js/runtime~main.a1c1c6e2.js"
+  },
+  {
+    "revision": "b8e8a42a26904d206453a543f2338104",
+    "url": "/CLickyGame/static/media/Background1.b8e8a42a.jpg"
+  },
+  {
+    "revision": "9a7db2e4cd063ddefad754f345b18f23",
+    "url": "/CLickyGame/index.html"
+  }
+];
